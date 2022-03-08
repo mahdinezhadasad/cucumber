f79950d1cca655fd799fbc773247afa81378cc4e
mahdinezhadasad <mahdinezhadasad@gmail.com>
2022-03-08 22:57:25 +0100

    Initial commit

---
+++ src/test/java/com/example/cucumber/CucumberIntegrationTest.java
@@ -0,0 +1,17 @@
+package com.example.cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources")
+public class CucumberIntegrationTest extends SpringIntegrationTest{
+}
